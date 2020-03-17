d196b34f7b26a276984c74ea6ff1e850c67431af
lianyizhishui <49826378+lianyizhishui@users.noreply.github.com>
2020-03-17 10:49:31 +0800

    simplepeople

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simplepeople
+{
+    class People {
+        private string name;
+        private int age;
+        public People(string name,int age) {
+            this.name = name;
+            this.age = age;
+        }
+        public People() : this("", -1) { }
+        public People(string name) {
+            this.name = name;
+        }
+       public People(int age) {
+            this.age = age;
+
+        }
+        public void ShowInfo() {
+            System.Console.WriteLine($"{name} is {age} years old");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            People yang = new People("yang",20);
+            yang.ShowInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
